57dd30576e2ebe8cbfc7cf7ea3484b5382f96e66
Linas Kriščiukaitis <l.krisciukaitis@users.noreply.github.com>
2021-03-09 23:21:30 +0200

    Initial commit

---
+++ js/01-01-pratimai.js
@@ -0,0 +1,76 @@
+// KINTAMUJU INICIJAVIMAS//
+
+//1//
+const pirmasSkaicius = 15;
+console.log(pirmasSkaicius)
+
+const antrasSkaicius = 69;
+console.log(antrasSkaicius)
+
+const treciaSkaicius = 420;
+console.log(treciaSkaicius)
+
+//2//
+const pirmasZodis = 'Vaikas';
+console.log(pirmasZodis);
+
+const antrasZodis = 'gavo';
+console.log(antrasZodis);
+
+const treciasZodis = 'dovanu';
+console.log(treciasZodis);
+
+//3//
+const skaiciai1 = [1, 3, 5, 7, 9];
+console.log(skaiciai1)
+
+const skaiciai2 = [2, 4, 6, 8, 10];
+console.log(skaiciai2)
+
+const skaiciai3 = [3, 15, 10, 7, 22];
+console.log(skaiciai3)
+
+//4//
+const raides1 = ['L', 'i', 'n', 'a', 's'];
+console.log(raides1);
+
+const raides2 = ['T', 'o', 'm', 'a', 's'];
+console.log(raides2)
+
+const raides3 = ['I', 'g', 'n', 'a', 's'];
+console.log(raides3)
+
+console.log('-------------------------')
+//VAIKSMAI SU KINTAMAISIAIS//
+
+//1//
+
+const suma = pirmasSkaicius + antrasSkaicius + treciaSkaicius;
+console.log(suma);
+
+//2//
+const zodziuJunginys = pirmasZodis+ ' ' + antrasZodis + ' ' + treciasZodis;
+console.log(zodziuJunginys);
+
+//3//
+const skaiciuVerte1 = skaiciai1[0] - skaiciai1[1] + skaiciai1[2] - skaiciai1[3] + skaiciai1[4];
+console.log(skaiciuVerte1);
+
+const skaiciuVerte2 = skaiciai2[0] - skaiciai2[1] + skaiciai2[2] - skaiciai2[3] + skaiciai2[4];
+console.log(skaiciuVerte2);
+
+
+const skaiciuVerte3 = skaiciai3[0] - skaiciai3[1] + skaiciai3[2] - skaiciai3[3] + skaiciai3[4];
+console.log(skaiciuVerte3);
+
+//4//
+
+const raidziuJunginys1 = raides1[0] + raides1[1] + raides1[2] + raides1[3] + raides1[4];
+const raidziuJunginys2 = raides2[0] + raides2[1] + raides2[2] + raides2[3] + raides2[4];
+const raidziuJunginys3 = raides3[0] + raides3[1] + raides3[2] + raides3[3] + raides3[4];
+
+const sujungtasSarasas = raidziuJunginys3 + ', ' + raidziuJunginys2 + ', ' + raidziuJunginys1;
+console.log(sujungtasSarasas);
+
+//KINTAMUJU PALYGINIMAS//
+
