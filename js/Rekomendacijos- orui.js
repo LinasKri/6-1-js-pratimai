7cf19a0574ffc83194487a4b8f501d4aca0b3a85
Linas Kriščiukaitis <l.krisciukaitis@users.noreply.github.com>
2021-03-14 19:54:33 +0200

    else if pratimas apie orą

---
+++ js/Rekomendacijos- orui.js
@@ -0,0 +1,26 @@
+// PASIRUOSIMAS PAGAL ORO SALYGAS //
+
+const oras = 'Stovi vilkas';
+
+if (oras === 'Šviečia saulė') {
+    console.log('Šortai su marškineliais');
+} else if (oras === 'Nešviečia saulė') {
+    console.log('Uzsidek svarkeli ir ilgesnes kelnes');
+} else if (oras === 'Lyja') {
+    console.log('Nepamiršk lietpalčio ar lietsargio');
+} else if (oras === 'Nelyja') {
+    console.log('Rekomenduoju atsižvelgti į temperatūrą')
+} else if (oras === 'Griuna dangus') {
+    console.log('Tau sakes, Seniukai 😬😬')
+} else if (oras === 'Stovi vilkas') {
+    console.log('Turėk su savimi šautuvą🔫🔫');
+} else if (oras === 'Nėra vilko') {
+    console.log('Vis tiek turėk šautuvą, nes vilkas gal už kampo');
+} else {
+    console.log('Kur tu gyveni??? 🤔🤔🤔');
+}
+
+/*
+- lauke šviečia / nešviečia saulė
+- lyja / nelyja
+- stovi vilkas / jo nėra */
