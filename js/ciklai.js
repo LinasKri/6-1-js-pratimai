0da103a027aab21306ea17331ce1414bd51d1990
Linas Kriščiukaitis <l.krisciukaitis@users.noreply.github.com>
2021-03-17 19:24:25 +0200

    Fix

--- js/ciklai.js
+++ js/ciklai.js
@@ -76,3 +76,45 @@ console.log('---------------------');
     ///// 3 /////
 
     
+/*
+Suskaičiuoti, kiek nurodytame intervale yra skaičių, kurie dalijasi be liekanos iš 3, 5 ir 7 atskirai:
+
+Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 3 yra 4 vienetai.
+Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 5 yra 3 vienetai.
+Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 7 yra 2 vienetai.
+
+a) 0 - 11;
+b) 8 - 31;
+c) -18 - 18;
+
+*/
+
+
+const tarp = -18;
+const ir = 18;
+const daliklis = 3; // 5; // 7
+let kiekis = 0;
+
+for (let i = tarp; i <= ir; i++) {
+    if (i % daliklis === 0) {
+        kiekis++;
+    }  
+}
+const atsakymas = `Skaičių intervale tarp ${tarp} ir ${ir}, besidalijančių be liekanos iš ${daliklis} yra ${kiekis} vienetai.`
+console.log(atsakymas);
+
+
+/*
+
+intervale 0 - 20   daliklis 5
+
+5, 10, 15, 20
+i += daliklis  (vietoj i++)
+
+
+intervale 11 - 20   daliklis 5    ir yra (iki-nuo) / daliklis + 1
+
+15, 20
+i += daliklis   (vietoj i++)
+
+*/
